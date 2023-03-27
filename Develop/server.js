aac3115a2b34b3ba68ddb3c2987656d1ab317e01
msgloriayang <yanggloria91@gmail.com>
2023-03-26 20:09:05 -0500

    added dependencies and port in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,66 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
 
-app.use(express.static('public'));
-// parse incoming string or array data
-app.use(express.urlencoded({extended: true}));
-// parse incoming JSON data
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.usa(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.static("../develop/public"));
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}. Welcome!`);
-  });
+app.get("/api/notes", function(req, res) {
+  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+    notes = [].concat(JSON.parse(data))
+    res.json(notes);
+  })
+});
+
+app.post("/api/notes", function(req, res) {
+  const note = req.body;
+  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+    const notes = [].concat(JSON.parse(data));
+    note.id = notes.length + 1
+    notes.push(note);
+    return notes
+  }).then(function(notes) {
+    writeFileAsync("./develop/dv/dv.json", JSON.stringify(notes))
+    res.json(notes);
+  })
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+  const idToDelete = parseInt(req.params.id);
+  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+    const notes = [].concat(JSON.parse(data));
+    const newNotesData = []
+    for (let i = 0; i<notes.length; i++) {
+      if(idToDelete !== notes[i].id) {
+        newNotesData.push(notes[i])
+      }
+    }
+  return newNotesData
+  }).then(function(notes) {
+    writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+    res.send('Saved success!');
+  })
+})
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(_dirname, "./develop/public/notes.html"));
+});
+app.get("/", function(req, res) {
+  res.sendFiles(path.join(_dirname, "./develop/public/index.html"));
+});
+app.get("+", function(req, res) {
+  res.sendFiles(path.join(_dirname, "./develop/public/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
