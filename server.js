70a2c9ad3cd795dedce4a954a44a8cb90de0013a
msgloriayang <yanggloria91@gmail.com>
2023-03-29 08:00:14 -0500

    fixed syntax errors and deleted develop folder

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3001;
 
-app.usa(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("../develop/public"));
+app.use(express.static("public"));
 
 app.get("/api/notes", function(req, res) {
-  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+  readFileAsync("./db/db.json", "utf8").then(function(data) {
     notes = [].concat(JSON.parse(data))
     res.json(notes);
   })
@@ -23,20 +23,20 @@ app.get("/api/notes", function(req, res) {
 
 app.post("/api/notes", function(req, res) {
   const note = req.body;
-  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+  readFileAsync("./db/db.json", "utf8").then(function(data) {
     const notes = [].concat(JSON.parse(data));
     note.id = notes.length + 1
     notes.push(note);
     return notes
   }).then(function(notes) {
-    writeFileAsync("./develop/dv/dv.json", JSON.stringify(notes))
+    writeFileAsync("./db/db.json", JSON.stringify(notes))
     res.json(notes);
   })
 });
 
 app.delete("/api/notes/:id", function(req, res) {
   const idToDelete = parseInt(req.params.id);
-  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+  readFileAsync("./db/db.json", "utf8").then(function(data) {
     const notes = [].concat(JSON.parse(data));
     const newNotesData = []
     for (let i = 0; i<notes.length; i++) {
@@ -46,20 +46,20 @@ app.delete("/api/notes/:id", function(req, res) {
     }
   return newNotesData
   }).then(function(notes) {
-    writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+    writeFileAsync("./db/db.json", JSON.stringify(notes))
     res.send('Saved success!');
   })
 })
 
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(_dirname, "./develop/public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 app.get("/", function(req, res) {
-  res.sendFiles(path.join(_dirname, "./develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-app.get("+", function(req, res) {
-  res.sendFiles(path.join(_dirname, "./develop/public/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
