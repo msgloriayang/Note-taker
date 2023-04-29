0c392f5faad4e4678d4f2a64a91c129d9d3d8dce
msgloriayang <yanggloria91@gmail.com>
2023-04-29 18:04:00 -0500

    fixed note.html page

---
+++ routes/notes.js
@@ -0,0 +1,34 @@
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const { readFromFile, readAndAppend } = require("../helpers/fsutils");
+
+// GET route for showing notes
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+// POST route for saving a note
+notes.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    res.json(response);
+  }
+});
+
+// TODO: DELETE route for deleting a note
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const util = require("util");
+const api = require("./routes/index.js");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
