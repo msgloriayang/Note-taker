d892b3a07d6133b1df720bace031a40c4c867076
msgloriayang <yanggloria91@gmail.com>
2023-04-29 17:52:42 -0500

    fixed href for notetaker navbar

--- server.js
+++ server.js
@@ -57,9 +57,9 @@ app.get("/notes", function(req, res) {
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./public/index.html"));
-// });
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
